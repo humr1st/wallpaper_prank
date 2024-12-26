c81ff758fda9ec615ede2142a58b409510629325
humr1st <132745036+humr1st@users.noreply.github.com>
2024-12-26 13:18:57 +0500

    Update prank.ps1

--- prank.ps1
+++ prank.ps1
@@ -1,6 +1,6 @@
 $client = new-object System.Net.WebClient
 
-$client.DownloadFile("https://tr3.cbsistatic.com/hub/i/2014/05/15/f8964afd-bd82-4e0e-bcbe-e927363dcdc1/3b858e39e2cf183b878f54cad0073a67/codedoge.jpg" , "doge.jpg")
+$client.DownloadFile("https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/383px-Flag_of_Russia.svg.png" , "doge.jpg")
 reg add "HKCU\\Control Panel\\Desktop\" /v WallPaper /d "%USERPROFILE%\\doge.jpg" /f 
 
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
