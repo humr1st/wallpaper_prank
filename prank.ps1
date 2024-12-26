528185c25b47805853f799c979b35bf58490ab41
humr1st <132745036+humr1st@users.noreply.github.com>
2024-12-26 13:09:24 +0500

    Update prank.ps1

--- prank.ps1
+++ prank.ps1
@@ -1,16 +1,19 @@
-# URL изображения
+# Указываем URL изображения
 $ImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/383px-Flag_of_Russia.svg.png"
 
-# Путь для сохранения изображения на компьютере
+# Путь для сохранения изображения
 $ImagePath = "$env:USERPROFILE\putin.jpg"
 
-# Скачивание изображения
+# Скачиваем изображение
 $client = New-Object System.Net.WebClient
 $client.DownloadFile($ImageUrl, $ImagePath)
 
-# Установка обоев через реестр
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d $ImagePath /f
+# Изменяем настройки реестра для обоев
+$regKey = "HKCU\Control Panel\Desktop"
+$regValue = "Wallpaper"
+Set-ItemProperty -Path $regKey -Name $regValue -Value $ImagePath
 
-# Обновление обоев
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+# Применяем изменения
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
 
+Write-Host "Обои установлены: $ImagePath"
